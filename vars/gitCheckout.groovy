93f6afca35fcf1de53742f22f340694fa517fbfe
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-04 00:34:24 +0530

    fixed commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,8 @@
-#some_code
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
