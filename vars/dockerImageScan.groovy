f8a9ff84270d4e32cee8ec15b5bdbd07b92dbe0f
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-05 02:31:51 +0530

    ECR related changes

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,11 +1,17 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+//     sh """
+//       trivy image ${hubUser}/${project}:latest > scan.txt
+//       cat scan.txt 
+//       """
+// }
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
     sh """
-      trivy image ${hubUser}/${project}:latest > scan.txt
-      cat scan.txt 
-      """
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan-${BUILD_NUMBER}.txt
+    cat scan-${BUILD_NUMBER}.txt
+    """
 }
-
-
 /*
 #!/bin/bash
 
