6f06650b49de11439dd577955de00eaa23115a74
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-04 23:37:49 +0530

    quality gate analysis

--- vars/qualityGatestatus.groovy
+++ vars/qualityGatestatus.groovy
@@ -1,3 +1,7 @@
 def call(credentialId){
 waitForQualityGate abortPipeline: false, credentialsId: credentialId
 }
+
+
+
+//while doing quality gate status check sonarqube needs to authenticate with jenkins for passing on information to jenkins reg quality status checks.
