e5737776c938a9a66317d24dbd377fb9071613c0
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-04 21:11:05 +0530

    added mvnIntegrationTest stage

--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,3 +1,19 @@
 def call(){
     sh 'mvn verify -DskipUnitTests'
 }
+
+/*
+No problem, let's break it down further:
+
+1. **Maven**: Maven is a popular build tool used primarily for Java projects. It manages the build process, dependencies, and project lifecycle.
+
+2. **Maven Build Lifecycle**: Maven has a predefined build lifecycle consisting of phases. The most common ones are `clean`, `compile`, `test`, `package`, `install`, and `verify`. Each phase represents a specific step in the build process.
+
+3. **Integration Testing**: In software testing, there are different types of tests, such as unit tests and integration tests. Unit tests focus on testing individual units or components of the code in isolation, while integration tests focus on testing how different units or components work together as a whole.
+
+4. **Maven Verify Phase**: The `verify` phase in Maven is part of the build lifecycle. It comes after the `test` phase, which is where unit tests are executed. The `verify` phase is typically used for running integration tests after the unit tests have passed successfully.
+
+5. **-DskipUnitTests**: This is a Maven command-line option that can be used to skip the execution of unit tests. By using this option, the trainer is instructing Maven not to run the unit tests during the `verify` phase.
+
+So, when you run `sh 'mvn verify -DskipUnitTests'`, it means Maven will perform the build up to the `verify` phase, but it will skip the execution of unit tests. This allows you to focus only on the integration tests, which test how different parts of your application work together in a real-world scenario.
+*/
